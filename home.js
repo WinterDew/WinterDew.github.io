c96c7b88ce17a13a228a5ee6bbf4b7491cd0a588
Tatva Agarwal <tatva.agarwal.2006@gmail.com>
2024-06-30 16:46:09 +0530

    Bug Fixes and better config

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 
 var file = (window.innerWidth < 1000) ? "res/particlesjs-config-mobile.json" : "res/particlesjs-config.json";
 
-particlesJS.load('particles-js', 'res/particlesjs-config.json', function() {
+particlesJS.load('particles-js', file, function() {
     console.log(`${file} loaded`);
 });
